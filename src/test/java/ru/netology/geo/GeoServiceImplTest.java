5bb860a15ff73aa3065a6fe33c45d4fa43084a75
Garrik Ten <ten@otux.ru>
2023-03-04 23:16:29 +0300

    edited pom.xml

---
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -0,0 +1,8 @@
+package ru.netology.geo;
+/**
+ * GeoServiceImplTest
+ */
+public class GeoServiceImplTest {
+
+	
+}
---
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -0,0 +1,5 @@
+package ru.netology.sender;
+
+public class MessageSenderImplTest {
+	
+}
