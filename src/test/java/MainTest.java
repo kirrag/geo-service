411ad25aa644691cf4d8c716aa29c44e6f44e670
Garrik Ten <ten@otux.ru>
2023-03-06 22:02:03 +0300

    geo-service tests v2.0

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -17,72 +17,90 @@ import ru.netology.sender.MessageSenderImpl;
 
 import org.junit.Test;
 
+//import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class MainTest {
 
 	@Test
 	public void messageSenderImplTest_Russian() {
-        GeoService geoService = Mockito.mock(GeoServiceImpl.class);
+		GeoService geoService = Mockito.mock(GeoServiceImpl.class);
 		Mockito.when(geoService.byIp(GeoServiceImpl.MOSCOW_IP))
-			.thenReturn(new Location("Moscow", Country.RUSSIA, "Lenina", 15));
+				.thenReturn(new Location("Moscow", Country.RUSSIA, "Lenina", 15));
 
-        LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
+		LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
 		Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
 
-        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+		MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
 
-        Map<String, String> headers = new HashMap<String, String>();
+		Map<String, String> headers = new HashMap<String, String>();
 
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, GeoServiceImpl.MOSCOW_IP);
+		headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, GeoServiceImpl.MOSCOW_IP);
 
-        String actual = messageSender.send(headers);
+		String actual = messageSender.send(headers);
 
 		String expected = "Добро пожаловать";
 
 		Assertions.assertEquals(expected, actual);
 	}
+
 	@Test
 	public void messageSenderImplTest_NotRussian() {
-        GeoService geoService = Mockito.mock(GeoServiceImpl.class);
+		GeoService geoService = Mockito.mock(GeoServiceImpl.class);
 		Mockito.when(geoService.byIp(GeoServiceImpl.NEW_YORK_IP))
-			.thenReturn(new Location("New York", Country.USA, " 10th Avenue", 32));
+				.thenReturn(new Location("New York", Country.USA, " 10th Avenue", 32));
 
-        LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
+		LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
 		Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
 
-        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+		MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
 
-        Map<String, String> headers = new HashMap<String, String>();
+		Map<String, String> headers = new HashMap<String, String>();
 
-        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, GeoServiceImpl.NEW_YORK_IP);
+		headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, GeoServiceImpl.NEW_YORK_IP);
 
-        String actual = messageSender.send(headers);
+		String actual = messageSender.send(headers);
 
 		String expected = "Welcome";
 
 		Assertions.assertEquals(expected, actual);
 	}
-	
-	@Test
-	public void geoServiceImplTest() {
+	@ParameterizedTest
+	@ValueSource(strings = { "127.0.0.1", "172.0.32.11", "96.44.183.149", "172.10.10.1", "96.10.10.1" })
+	public void geoServiceImplTestByIP(String argument) {
+		
 		GeoService geoService = new GeoServiceImpl();
 
-      	Country actualCountry = geoService.byIp(GeoServiceImpl.MOSCOW_IP).getCountry();
-		String actualCountryName = actualCountry.toString();
-		String expectedCountryName = "RUSSIA";
-		Assertions.assertEquals(expectedCountryName, actualCountryName);
-		String actualCity = geoService.byIp(GeoServiceImpl.MOSCOW_IP).getCity();
-		String expectedCity = "Moscow";
-		Assertions.assertEquals(expectedCity, actualCity);
-		String actualStreet = geoService.byIp(GeoServiceImpl.MOSCOW_IP).getStreet();
-		String expectedStreet = "Lenina";
-		Assertions.assertEquals(expectedStreet, actualStreet);
-		int actualBuiling = geoService.byIp(GeoServiceImpl.MOSCOW_IP).getBuiling();
-		int expectedBuiling = 15;
-		Assertions.assertEquals(expectedBuiling, actualBuiling);
+		Location actualCountryLocation = geoService.byIp(argument);
+
+		Location expectedCountryLocation = null;
+
+		if (argument.equals("127.0.0.1")) {
+			expectedCountryLocation = new Location(null, null, null, 0);
+		} else if (argument.equals("172.0.32.11")) {
+			expectedCountryLocation = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+		} else if (argument.equals("96.44.183.149")) {
+			expectedCountryLocation = new Location("New York", Country.USA, " 10th Avenue", 32);
+		} else if (argument.startsWith("172.")) {
+			expectedCountryLocation = new Location("Moscow", Country.RUSSIA, null, 0);
+		} else if (argument.startsWith("96.")) {
+			expectedCountryLocation = new Location("New York", Country.USA, null, 0);
+		}
+		Assertions.assertEquals(expectedCountryLocation, actualCountryLocation);
+
 	}
 
 	@Test
-	public void LocalizationServiceImpl() {
+	public void geoServiceImplTestByCoordinateThrowsException() {
+		GeoService geoService = new GeoServiceImpl();
+		Assertions.assertThrows(Exception.class, () -> {
+			geoService.byCoordinates(49.5, 39.6);
+		});
+	}
+
+	@Test
+	public void localizationServiceImplTestLocale() {
 		LocalizationService localizationService = new LocalizationServiceImpl();
 
 		String actualLocale = localizationService.locale(Country.RUSSIA);
@@ -91,4 +109,3 @@ public class MainTest {
 		Assertions.assertEquals(expectedLocale, actualLocale);
 	}
 }
-
